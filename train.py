1bbae20a1e28af28ff0d1f2df4580ed854f6dc5f
Tan Pei Yu <120084583+tanpeiyu1209tpy@users.noreply.github.com>
2025-11-06 11:59:53 +0800

    Refactor training setup and dataset handling (2)
    Removed unnecessary load_dataset parameter and directly instantiated custom datasets for training and validation. Updated training parameters and added warnings for training step recommendations.

--- train.py
+++ train.py
@@ -579,3 +579,234 @@ if __name__ == "__main__":
 '''
 
 # ------------------ pair + classification ----------------------------
+
+import os
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from nets.siamese import Siamese
+from utils.callbacks import LossHistory
+from utils.dataloader import SiameseDataset, dataset_collate
+# 移除了 load_dataset，因为它不再需要
+from utils.utils import (download_weights, get_lr_scheduler,
+                         set_optimizer_lr, show_config)
+from utils.utils_fit import fit_one_epoch
+
+if __name__ == "__main__":
+    #----------------------------------------------------#
+    #   是否使用Cuda
+    #   没有GPU可以设置成False
+    #----------------------------------------------------#
+    Cuda            = True
+    #---------------------------------------------------------------------#
+    #   distributed     用于指定是否使用单机多卡分布式运行
+    #---------------------------------------------------------------------#
+    distributed     = False
+    #---------------------------------------------------------------------#
+    #   sync_bn     是否使用sync_bn，DDP模式多卡可用
+    #---------------------------------------------------------------------#
+    sync_bn         = False
+    #---------------------------------------------------------------------#
+    #   fp16        是否使用混合精度训练
+    #---------------------------------------------------------------------#
+    fp16            = False
+    #----------------------------------------------------#
+    #   数据集存放的路径
+    #   请修改为你实际清洗后的数据根目录！
+    #----------------------------------------------------#
+    dataset_path    = "/kaggle/working/siamese_data_cleaned"
+    #----------------------------------------------------#
+    #   输入图像的大小，默认为105,105,3
+    #----------------------------------------------------#
+    input_shape     = [224, 224]
+    
+    # train_own_data 参数已废弃，删除
+
+    #-------------------------------#
+    #   用于指定是否使用VGG预训练权重
+    #-------------------------------#
+    pretrained      = True
+    #----------------------------------------------------------------------------------------------------------------------------#
+    #   如果训练过程中存在中断训练的操作，可以将model_path设置成logs文件夹下的权值文件，将已经训练了一部分的权值再次载入。
+    #   同时修改下方的 冻结阶段 或者 解冻阶段 的参数，来保证模型epoch的连续性。
+    #----------------------------------------------------------------------------------------------------------------------------#
+    model_path      = ""
+
+    #------------------------------------------------------#
+    #   训练参数
+    #   Init_Epoch      模型当前开始的训练世代
+    #   Epoch           模型总共训练的epoch
+    #   batch_size      每次输入的图片数量
+    #------------------------------------------------------#
+    Init_Epoch      = 0
+    Epoch           = 5
+    batch_size      = 4
+    
+    #------------------------------------------------------------------#
+    #   其它训练参数：学习率、优化器、学习率下降有关
+    #------------------------------------------------------------------#
+    Init_lr         = 1e-2
+    Min_lr          = Init_lr * 0.01
+    optimizer_type  = "sgd"
+    momentum        = 0.9
+    weight_decay    = 5e-4
+    lr_decay_type   = 'cos'
+    save_period     = 10
+    save_dir        = 'logs'
+    num_workers     = 4
+
+    #------------------------------------------------------#
+    #   设置用到的显卡
+    #------------------------------------------------------#
+    ngpus_per_node  = torch.cuda.device_count()
+    if distributed:
+        dist.init_process_group(backend="nccl")
+        local_rank  = int(os.environ["LOCAL_RANK"])
+        rank        = int(os.environ["RANK"])
+        device      = torch.device("cuda", local_rank)
+        if local_rank == 0:
+            print(f"[{os.getpid()}] (rank = {rank}, local_rank = {local_rank}) training...")
+            print("Gpu Device Count : ", ngpus_per_node)
+    else:
+        device          = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        local_rank      = 0
+
+    if pretrained:
+        if distributed:
+            if local_rank == 0:
+                download_weights("vgg16")  
+            dist.barrier()
+        else:
+            download_weights("vgg16")  
+
+    model = Siamese(input_shape, pretrained)
+    if model_path != '':
+        if local_rank == 0:
+            print('Load weights {}.'.format(model_path))
+        model_dict      = model.state_dict()
+        pretrained_dict = torch.load(model_path, map_location = device)
+        load_key, no_load_key, temp_dict = [], [], {}
+        for k, v in pretrained_dict.items():
+            if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
+                temp_dict[k] = v
+                load_key.append(k)
+            else:
+                no_load_key.append(k)
+        model_dict.update(temp_dict)
+        model.load_state_dict(model_dict)
+        if local_rank == 0:
+            print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
+            print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
+    
+    #----------------------#
+    #   获得损失函数
+    #----------------------#
+    loss_match_fn = nn.BCEWithLogitsLoss()
+    loss_cls_fn = nn.CrossEntropyLoss()
+
+  
+    if local_rank == 0:
+        loss_history = LossHistory(save_dir, model, input_shape=input_shape)
+    else:
+        loss_history = None
+        
+    if fp16:
+        from torch.cuda.amp import GradScaler as GradScaler
+        scaler = GradScaler()
+    else:
+        scaler = None
+
+    model_train     = model.train()
+    if sync_bn and ngpus_per_node > 1 and distributed:
+        model_train = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model_train)
+    elif sync_bn:
+        print("Sync_bn is not support in one gpu or not distributed.")
+
+    if Cuda:
+        if distributed:
+            model_train = model_train.cuda(local_rank)
+            model_train = torch.nn.parallel.DistributedDataParallel(model_train, device_ids=[local_rank], find_unused_parameters=True)
+        else:
+            model_train = torch.nn.DataParallel(model)
+            cudnn.benchmark = True
+            model_train = model_train.cuda()
+
+    #----------------------------------------------------#
+    #   修改点：直接实例化自定义数据集
+    #   不再使用 load_dataset 和 train_ratio
+    #----------------------------------------------------#
+    # 注意：这里为了简化，训练集和验证集使用了同一个数据目录。
+    # 在正式训练中，建议你划分出一个独立的验证集目录传入 val_dataset
+    train_dataset   = SiameseDataset(input_shape, dataset_path, random_flag=True, autoaugment_flag=True)
+    val_dataset     = SiameseDataset(input_shape, dataset_path, random_flag=False, autoaugment_flag=False)
+    
+    num_train   = len(train_dataset)
+    num_val     = len(val_dataset)
+
+    if local_rank == 0:
+        # 移除了 show_config 中不再适用的参数
+        show_config(
+            model_path = model_path, input_shape = input_shape, \
+            Init_Epoch = Init_Epoch, Epoch = Epoch, batch_size = batch_size, \
+            Init_lr = Init_lr, Min_lr = Min_lr, optimizer_type = optimizer_type, momentum = momentum, lr_decay_type = lr_decay_type, \
+            save_period = save_period, save_dir = save_dir, num_workers = num_workers, num_train = num_train, num_val = num_val
+        )
+        wanted_step = 3e4 if optimizer_type == "sgd" else 1e4
+        total_step  = num_train // batch_size * Epoch
+        if total_step <= wanted_step:
+            wanted_epoch = wanted_step // (num_train // batch_size) + 1
+            print("\n\033[1;33;44m[Warning] 使用%s优化器时，建议将训练总步长设置到%d以上。\033[0m"%(optimizer_type, wanted_step))
+            print("\033[1;33;44m[Warning] 本次运行的总训练数据量为%d，batch_size为%d，共训练%d个Epoch，计算出总训练步长为%d。\033[0m"%(num_train, batch_size, Epoch, total_step))
+            print("\033[1;33;44m[Warning] 由于总训练步长为%d，小于建议总步长%d，建议设置总世代为%d。\033[0m"%(total_step, wanted_step, wanted_epoch))
+
+    if True:
+        nbs             = 64
+        lr_limit_max    = 1e-3 if optimizer_type == 'adam' else 1e-1
+        lr_limit_min    = 3e-4 if optimizer_type == 'adam' else 5e-4
+        Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
+        Min_lr_fit      = min(max(batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
+
+        optimizer = {
+            'adam'  : optim.Adam(model.parameters(), Init_lr_fit, betas = (momentum, 0.999), weight_decay = weight_decay),
+            'sgd'   : optim.SGD(model.parameters(), Init_lr_fit, momentum=momentum, nesterov=True, weight_decay = weight_decay)
+        }[optimizer_type]
+
+        lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, Epoch)
+        
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+        
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
+
+        if distributed:
+            train_sampler   = torch.utils.data.distributed.DistributedSampler(train_dataset, shuffle=True,)
+            val_sampler     = torch.utils.data.distributed.DistributedSampler(val_dataset, shuffle=False,)
+            batch_size      = batch_size // ngpus_per_node
+            shuffle         = False
+        else:
+            train_sampler   = None
+            val_sampler     = None
+            shuffle         = True
+
+        gen     = DataLoader(train_dataset, shuffle=shuffle, batch_size=batch_size, num_workers=num_workers, pin_memory=True,
+                                drop_last=True, collate_fn=dataset_collate, sampler=train_sampler)
+        gen_val = DataLoader(val_dataset, shuffle=shuffle, batch_size=batch_size, num_workers=num_workers, pin_memory=True,
+                                drop_last=True, collate_fn=dataset_collate, sampler=val_sampler)
+
+        for epoch in range(Init_Epoch, Epoch):
+            if distributed:
+                train_sampler.set_epoch(epoch)
+                
+            set_optimizer_lr(optimizer, lr_scheduler_func, epoch)
+            
+            fit_one_epoch(model_train, model, loss_match_fn, loss_cls_fn, loss_history,, optimizer, epoch, epoch_step, epoch_step_val, gen, gen_val, Epoch, Cuda, fp16, scaler, save_period, save_dir, local_rank)
+
+        if local_rank == 0:
+            loss_history.writer.close()
