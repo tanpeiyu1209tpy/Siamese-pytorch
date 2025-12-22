3a58f94d635cc68f5f871118f6db12d983b0ffaf
Tan Pei Yu <120084583+tanpeiyu1209tpy@users.noreply.github.com>
2025-12-22 16:11:14 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -9,7 +9,11 @@ import argparse
 import re
 
 transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
 # 解析 patch 文件名
